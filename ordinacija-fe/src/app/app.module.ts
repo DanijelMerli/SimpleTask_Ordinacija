a5bb64c82d698a9dad8fabddf5c87fae2124d3a3
Danijel Merli <danijel.merli@gmail.com>
2021-09-26 18:21:27 +0200

    home and navbar components

--- ordinacija-fe/src/app/app.module.ts
+++ ordinacija-fe/src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
