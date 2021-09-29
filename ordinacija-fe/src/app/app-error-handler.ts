9f294b577cde57a9d5b7f7961404cbc19a537da4
Danijel Merli <danijel.merli@gmail.com>
2021-09-29 14:23:51 +0200

    more user friendly errors

--- ordinacija-fe/src/app/app-error-handler.ts
+++ ordinacija-fe/src/app/app-error-handler.ts
@@ -1,3 +1,4 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { ErrorHandler, Injectable } from '@angular/core';
 import { AlertifyService } from './services/alertify.service';
 
@@ -7,7 +8,14 @@ import { AlertifyService } from './services/alertify.service';
 export class AppErrorHandler implements ErrorHandler {
   constructor(private alertify: AlertifyService) {}
 
-  handleError(error: Error): void {
-    this.alertify.error(error.error);
+  handleError(error: any): void {
+    console.log(error);
+    if (error.error && typeof error.error == 'string') {
+      this.alertify.error(error.error);
+    } else {
+      this.alertify.error(
+        'Status code: ' + error.status + '</br>Message: ' + error.message
+      );
+    }
   }
 }
